a87f10b1cb0e89311f630bcd365358a05efb36c9
Muhammad_Egb <mhmad.m.ig2001@gmail.com>
2025-08-23 13:36:51 +0300

    Create CARD IN Home page

--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -23,5 +23,46 @@ export const useProductStore = create((set) => ({
         products: [...state.products, data.data]
   }));
         return {success: true, message: "Product created successfully"};
-}
+},
+  fetchProducts: async () => {
+    // Function to fetch products from the backend API
+    try {
+      const res = await fetch("/api/products");
+      const data = await res.json();
+      set({ products: data.data }); // Update the products state with fetched data
+    } catch (error) {
+      console.error("Failed to fetch products:", error);
+    }
+  },
+  deleteProduct: async (pid) => {
+    const res = await fetch(`/api/products/${pid}`, {
+        method: "DELETE",
+    });
+    const data = await res.json();
+    if (!data.success) {
+        return { success: false, message: data.message };
+    }
+    //update the ui immediately,without needing to refetch all products
+    set((state) => ({ products: state.products.filter((product) => product._id !== pid) }) );
+    return { success: true, message: data.message };
+  },
+  updateProduct: async (pid, updatedProduct) => {
+    const res = await fetch(`/api/products/${pid}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(updatedProduct)
+    });
+    const data = await res.json();
+    if (!data.success) {
+        return { success: false, message: data.message };
+    }
+    // Update the product in the state without needing a refetch
+    set((state) => ({
+        products: state.products.map((product) =>
+            product._id === pid ? data.data : product
+        )
+    }));
+  }
 }));
